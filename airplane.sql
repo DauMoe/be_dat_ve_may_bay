8e8a4ee3e2e37ffb6f308554e49be32b786f5dca
DauMoe <lehuyhoang1111999@gmail.com>
2022-03-27 20:19:33 +0700

    Create DB for MariaDB

--- airplane.sql
+++ airplane.sql
@@ -35,17 +35,59 @@ INSERT INTO `airplane` VALUES (1, 'BOEING-747', '300');
 INSERT INTO `airplane` VALUES (2, 'AIRBUS-A350', '150');
 
 -- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`  (
+     `id` int NOT NULL AUTO_INCREMENT,
+     `dob` date NULL DEFAULT NULL,
+     `email` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+     `email_verified` bit(1) NULL DEFAULT NULL,
+     `gender` bit(1) NULL DEFAULT NULL,
+     `password` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+     `phone` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+     `role` bit(1) NULL DEFAULT NULL,
+     `uid` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+     `username` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+     `verification_code` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+     PRIMARY KEY (`id`) USING BTREE,
+     UNIQUE INDEX `UK_ob8kqyqqgmefl0aco34akdtpe`(`email`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for location
+-- ----------------------------
+DROP TABLE IF EXISTS `location`;
+CREATE TABLE `location`  (
+     `LOCATION_ID` int UNSIGNED NOT NULL AUTO_INCREMENT,
+     `COUNTRY_CODE` int UNSIGNED NOT NULL,
+     `CITY_CODE` int UNSIGNED NOT NULL,
+     `COUNTRY_NAME` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+     `LONGITUDE` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+     `LATITUDE` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+     `city_id` int NULL DEFAULT NULL,
+     `city_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+     PRIMARY KEY (`LOCATION_ID`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of location
+-- ----------------------------
+INSERT INTO `location` VALUES (1, 10, 10, 'VIETNAM', '10', '11', NULL, NULL);
+INSERT INTO `location` VALUES (2, 221, 22, 'RUSSIA', '12', '23', NULL, NULL);
+
+-- ----------------------------
 -- Table structure for airport_geo
 -- ----------------------------
 DROP TABLE IF EXISTS `airport_geo`;
 CREATE TABLE `airport_geo`  (
-  `AIRPORT_GEO_ID` int UNSIGNED NOT NULL AUTO_INCREMENT,
-  `LOCATION_ID` int UNSIGNED NULL DEFAULT NULL,
-  `AIRPORT_NAME` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
-  `DESCRIPTION` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
-  PRIMARY KEY (`AIRPORT_GEO_ID`) USING BTREE,
-  INDEX `LOCATION_ID`(`LOCATION_ID`) USING BTREE,
-  CONSTRAINT `airport_geo_ibfk_1` FOREIGN KEY (`LOCATION_ID`) REFERENCES `location` (`LOCATION_ID`) ON DELETE CASCADE ON UPDATE RESTRICT
+    `AIRPORT_GEO_ID` int UNSIGNED NOT NULL AUTO_INCREMENT,
+    `LOCATION_ID` int UNSIGNED NULL DEFAULT NULL,
+    `AIRPORT_NAME` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+    `DESCRIPTION` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+    PRIMARY KEY (`AIRPORT_GEO_ID`) USING BTREE,
+    INDEX `LOCATION_ID`(`LOCATION_ID`) USING BTREE,
+    CONSTRAINT `airport_geo_ibfk_1` FOREIGN KEY (`LOCATION_ID`) REFERENCES `location` (`LOCATION_ID`) ON DELETE CASCADE ON UPDATE RESTRICT
 ) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
@@ -123,28 +165,6 @@ CREATE TABLE `flight_schedule`  (
 INSERT INTO `flight_schedule` VALUES (1, '2022-03-08 21:56:54.000000', '2022-03-08 23:56:54.000000', 'DN1234', 146);
 
 -- ----------------------------
--- Table structure for location
--- ----------------------------
-DROP TABLE IF EXISTS `location`;
-CREATE TABLE `location`  (
-  `LOCATION_ID` int UNSIGNED NOT NULL AUTO_INCREMENT,
-  `COUNTRY_CODE` int UNSIGNED NOT NULL,
-  `CITY_CODE` int UNSIGNED NOT NULL,
-  `COUNTRY_NAME` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
-  `LONGITUDE` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
-  `LATITUDE` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
-  `city_id` int NULL DEFAULT NULL,
-  `city_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
-  PRIMARY KEY (`LOCATION_ID`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Records of location
--- ----------------------------
-INSERT INTO `location` VALUES (1, 10, 10, 'VIETNAM', '10', '11', NULL, NULL);
-INSERT INTO `location` VALUES (2, 221, 22, 'RUSSIA', '12', '23', NULL, NULL);
-
--- ----------------------------
 -- Table structure for password_reset_token
 -- ----------------------------
 DROP TABLE IF EXISTS `password_reset_token`;
@@ -182,30 +202,11 @@ CREATE TABLE `ticket`  (
 INSERT INTO `ticket` VALUES (9, '1', 0, 1500000, '2', 0);
 
 -- ----------------------------
--- Table structure for user
--- ----------------------------
-DROP TABLE IF EXISTS `user`;
-CREATE TABLE `user`  (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `dob` date NULL DEFAULT NULL,
-  `email` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `email_verified` bit(1) NULL DEFAULT NULL,
-  `gender` bit(1) NULL DEFAULT NULL,
-  `password` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `phone` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `role` bit(1) NULL DEFAULT NULL,
-  `uid` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `username` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `verification_code` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE,
-  UNIQUE INDEX `UK_ob8kqyqqgmefl0aco34akdtpe`(`email`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
 -- Records of user
 -- ----------------------------
 INSERT INTO `user` VALUES (1, '0000-00-00', 'long', b'1', b'1', '', NULL, NULL, '0', NULL, '');
 INSERT INTO `user` VALUES (2, '0000-00-00', 'longpp', b'1', b'1', '', NULL, NULL, '0', NULL, 'ff278dd4-53fd-4450-950c-45a07233c119');
 INSERT INTO `user` VALUES (3, NULL, 'user1@gmail.com', b'1', NULL, '$2a$10$sYScRrUe6Tg4544AayFKk.igYE0zsfn2Xm8VH5nCSouDr81HsY8ai', NULL, b'0', 'c16ce9ec-a365-4217-af8c-b1eeee6da99a', 'Nguyen Van A', '09Jt0vFrcRLalqZrqz4mr4pBzmR7BMlJd9r9VM0PBGS06efUzjgbbFeZCxiy5EHa');
+
 
 SET FOREIGN_KEY_CHECKS = 1;
