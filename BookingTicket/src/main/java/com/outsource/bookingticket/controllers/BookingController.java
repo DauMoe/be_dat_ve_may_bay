b66b5089b2c32e62e9d1030943c4f869f7142abb
lenam30101999 <levunam30101999@gmail.com>
2022-04-24 14:49:48 +0700

    Merge controller

--- BookingTicket/src/main/java/com/outsource/bookingticket/controllers/BookingController.java
+++ BookingTicket/src/main/java/com/outsource/bookingticket/controllers/BookingController.java
@@ -9,14 +9,21 @@ import javax.mail.MessagingException;
 import java.io.UnsupportedEncodingException;
 
 @RestController
-@RequestMapping("/book/create")
+@RequestMapping("/book")
 public class BookingController extends BaseController {
 
     @CrossOrigin(maxAge = 3600, origins = "*")
-    @PostMapping
+    @PostMapping(path = "/create")
     public ResponseEntity<?> bookingFlight(@RequestHeader("Authorization") String token,
                                            @RequestBody BookingRequestDto requestDto) throws MessagingException, UnsupportedEncodingException {
         ResponseCommon responseCommon = bookingService.bookingFlight(token, requestDto);
         return ResponseEntity.ok(responseCommon);
+    }
+
+    @CrossOrigin(maxAge = 3600, origins = "*")
+    @PutMapping(path = "/cancel")
+    public ResponseEntity<?> cancelTicket(@RequestParam("ticket_id") Integer ticketId){
+        ResponseCommon response = cancelBookingService.cancelTicket(ticketId);
+        return ResponseEntity.ok(response);
     }
 }
