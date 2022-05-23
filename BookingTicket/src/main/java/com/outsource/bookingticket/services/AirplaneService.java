7adff0cfd44448fe860c3b76bd3c822a3d270047
lenam30101999 <levunam30101999@gmail.com>
2022-05-23 21:08:33 +0700

    Add more comment

--- BookingTicket/src/main/java/com/outsource/bookingticket/services/AirplaneService.java
+++ BookingTicket/src/main/java/com/outsource/bookingticket/services/AirplaneService.java
@@ -21,38 +21,49 @@ public class AirplaneService extends BaseService {
 
     // API thêm máy bay
     public ResponseEntity<?> addAirplane(AirplaneDTO airplaneDTO) {
+        // Kiểm tra xem đã tồn tại máy bay này chưa
         Optional<Airplane> existAirplane =
                 airplaneRepository.findByAirplaneNameAndBrand(airplaneDTO.getAirplaneName(), airplaneDTO.getBrand());
+        // Nếu đã tồn tại, trả ra lỗi như bên dưới
         if (existAirplane.isPresent()) {
             throw new ErrorException(MessageUtil.AIRPLANE_IS_EXIST);
         }
 
+        // Khởi tạo đối tượng để lưu vào DB
         Airplane saved = Airplane.builder()
                 .airplaneName(airplaneDTO.getAirplaneName())
                 .capacity(airplaneDTO.getCapacity())
                 .brand(airplaneDTO.getBrand())
                 .linkImgBrand(airplaneDTO.getLinkImgBrand())
                 .build();
+        // Lưu máy bay
         airplaneRepository.save(saved);
+
+        // Trả về thông báo thêm thành công
         return ResponseEntity.ok(Helper.createSuccessCommon(MessageUtil.INSERT_SUCCESS));
     }
 
     // Lấy hết danh sách máy bay
     public ResponseEntity<?> getAllAirplane() {
+        // Lấy hết danh sách máy bay
         List<Airplane> airplaneList = airplaneRepository.findAll();
 
+        // Kiểm tra nếu danh sách rỗng sẽ trả về thông báo
         if (CollectionUtils.isEmpty(airplaneList)) {
             throw new ErrorException(MessageUtil.AIRPLANE_IS_EMPTY);
         }
 
+        // Gán danh sách vừa tìm được để trả về hiển thị
         List<AirplaneDTO> resultList = airplaneList
                 .stream()
                 .map(this::mapToAirplaneDTO)
                 .collect(Collectors.toList());
 
+        // Trả về hiển thị
         return ResponseEntity.ok(Helper.createSuccessListCommon(new ArrayList<>(resultList)));
     }
 
+    // Hàm gán giá trị thuộc tính của Airplane sang AirplaneDTO
     private AirplaneDTO mapToAirplaneDTO(Airplane airplane) {
         return AirplaneDTO.builder()
                 .airplaneId(airplane.getAirplaneId())
